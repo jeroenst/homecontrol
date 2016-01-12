331f2094bfcdb8864b19e8ed25c34bf2b114b625
Jeroen Steenhuis <jeroen@j4m.nl>
2016-01-12 12:20:25 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -8,16 +8,12 @@ echo "homecontrol..."
 g++ -o homecontrol homecontrol.cpp -lrt
 
 echo "\n=== Installing Executeables ===\n"
-#sudo cp 433mhzdaemon /usr/sbin
-#sudo cp 433mhztool /usr/bin
-#sudo chmod +s /usr/bin/433mhztool
-#sudo cp kaku /usr/bin
-#sudo chmod +s /usr/bin/kaku
-#sudo cp action /usr/bin
-#sudo chmod +s /usr/bin/action
+sudo cp -v homecontroldaemon /usr/sbin
+sudo cp -v homecontrol /usr/bin
+#sudo chmod +s /usr/bin/homecontrol
 cd ..
 
 echo "\n=== Installing Webpages ===\n"
 rm -rf /var/www/homecontrol
-cp -R ./www /var/www/homecontrol
-echo "\n=== Finished, kaku&action installed in /usr/bin, type kaku or action at prompt to use it or goto http://<raspberrypip>/domotica  ==="
+cp -vR ./www /var/www/homecontrol
+echo "\n=== Finished, homecontrol installed in /usr/bin, type homecontrol at prompt to use it or goto http://<serverip>/homecontrol  ==="
