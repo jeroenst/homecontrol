e9be10add1182690556f277c63f089e6aedd9bd9
Jeroen Steenhuis <jeroen@j4m.nl>
2016-01-13 16:31:48 +0100

    When USB arduino is removed and reinserted again the deamon will reconnect to the arduino and continues to work.

--- daemon/homecontroldaemon.cpp
+++ daemon/homecontroldaemon.cpp
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <termios.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -22,7 +23,7 @@
 using namespace std;
 
 std::queue<string> arduino_queue;
-int fd, res, i;
+int fd = -1, res, i;
 int waitforanswer = 0;
 
 //
@@ -53,49 +54,12 @@ else
     printf ("Usage: homecontroldaemon <device like /dev/ttyUSB0>\n");
     exit(1);
 }
-//
-//open the device in non-blocking way (read will return immediately)
-fd = open(devicename, O_RDWR | O_NOCTTY | O_NONBLOCK);
-if (fd < 0)
-{
-perror(devicename);
-exit(1);
-}
-//
-//install the serial handler before making the device asynchronous
-//saio.sa_handler = signal_handler_IO;
-//sigemptyset(&saio.sa_mask);   //saio.sa_mask = 0;
-//saio.sa_flags = 0;
-//saio.sa_restorer = NULL;
-//sigaction(SIGIO,&saio,NULL);
-//
-// allow the process to receive SIGIO
-//fcntl(fd, F_SETOWN, getpid());
-//
-// make the file descriptor asynchronous
-//fcntl(fd, F_SETFL, FASYNC);
-//
-// set new port settings for canonical input processing
-newtio.c_cflag = B9600 | CRTSCTS | CS8 | CLOCAL | CREAD;
-newtio.c_iflag = IGNPAR;
-newtio.c_oflag = 0;
-newtio.c_lflag = 0;
-newtio.c_cc[VMIN]=1;
-newtio.c_cc[VTIME]=0;
-tcflush(fd, TCIFLUSH);
-tcsetattr(fd,TCSANOW,&newtio);
-//
-// loop while waiting for input. normally we would do something useful here
 
-
-
-        // Form the queue attributes
+        // Create message queue
         attr.mq_flags = 0; //O_NONBLOCK; // i.e mq_send will be block if message queue is full
         attr.mq_maxmsg = MQ_MAX_NUM_OF_MESSAGES;
         attr.mq_msgsize = MQ_MESSAGE_MAX_LENGTH;
         attr.mq_curmsgs = 0; // mq_curmsgs is dont care
-
-        // Create message queue
         system ("mkdir /dev/mqueue >/dev/null 2>&1");
         system ("mount -t mqueue none /dev/mqueue >/dev/null 2>&1");
         system (string("sudo chmod 666 /dev/mqueue" + string(MQ_NAME) + " >/dev/null 2>&1").c_str());
@@ -103,24 +67,26 @@ tcsetattr(fd,TCSANOW,&newtio);
         if( mqd != (mqd_t)-1 )
         {
                 printf(" Message Queue Opened\n");
-
                 printf(" Receiving message....\n");
                 while(1)
 		{
-		    fd_set set;
-		    struct timeval timeout;
 		    /* Initialize the file descriptor set. */
+		    fd_set set;
 		    FD_ZERO (&set);
 		    FD_SET (mqd, &set);
-		    FD_SET (fd, &set);
+		    if (fd >= 0) FD_SET (fd, &set);
 
 		    /* Initialize the timeout data structure. */
+		    struct timeval timeout;
 	            timeout.tv_sec = 5;
 		    timeout.tv_usec = 0;
-
+		    if (fd < 0)  timeout.tv_sec = 1;
+		    
+		    // Wait for timeout or data on filedescriptors
 		    if (select (FD_SETSIZE, &set, NULL, NULL, &timeout) == 0)
 		    {
                        // If timeout send next message to arduino if no response on previous command
+                       printf ("Select timeout!\n");
                        waitforanswer = 0;   		        
 		    }
 
@@ -128,31 +94,67 @@ tcsetattr(fd,TCSANOW,&newtio);
                     if(msg_len > 0) // if a message is received from message queueu put it in arduino queue
                     {
                         msg[msg_len] = '\0';
-                        printf(" Successfully received %d bytes: %s", (int)msg_len, msg);
+                        printf(" Received from message queue: %s",  msg);
                         arduino_queue.push(msg);
 
                     }
 
-                    int flags = fcntl(fd, F_GETFL, 0);
-                    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-                    char buf[4096];
-                    res = read(fd,buf,4096);
-                    if (res > 0)
+                    if (fd < 0)
                     {
-                          buf[res] = 0;
-                          printf(" Received from arduino: %s", buf);
-                          waitforanswer = 0;
+                        printf (" Opening device: %s...\n", devicename);
+                        //
+                        //open the device in non-blocking way (read will return immediately)
+                        fd = open(devicename, O_RDWR | O_NOCTTY | O_NONBLOCK);
+                        // set new port settings for canonical input processing
+                        int flags = fcntl(fd, F_GETFL, 0);
+                        newtio.c_cflag = B9600 | CRTSCTS | CS8 | CLOCAL | CREAD;
+                        newtio.c_iflag = IGNPAR;
+                        newtio.c_oflag = 0;
+                        newtio.c_lflag = 0;
+                        newtio.c_cc[VMIN]=1;
+                        newtio.c_cc[VTIME]=0;
+                        tcflush(fd, TCIFLUSH);
+                        tcsetattr(fd,TCSANOW,&newtio);
+                        fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+                    }
+                    
+                    
+                    if (fd >= 0)
+                    {
+                        char buf[4096];
+                        res = read(fd,buf,4096);
+                        if ((res < 0) && (errno != EWOULDBLOCK))
+                        {
+                                printf (" Error while reading arduino, device %s closed...\n", devicename);
+                                close(fd);
+                                fd = -1;
+                        }
+                        if (res > 0)
+                        {
+                              buf[res] = 0;
+                              printf(" Received from arduino: %s", buf);
+                              waitforanswer = 0;
+                        }
                     }
 
-                    if (waitforanswer == 0) //wait with sending until arduino has send an answer and is ready for next command
+                    //wait with sending until arduino has send an answer and is ready for next command
+                    if ((waitforanswer == 0) && (fd >= 0))
                     {
                         if (!arduino_queue.empty())
                         {
                                 string message = arduino_queue.front();
-                                arduino_queue.pop();
                                 printf (" Writing to arduino: %s", message.c_str());
-                                write (fd, message.c_str(), strlen(message.c_str()));
-                                waitforanswer = 1;
+                                if (write (fd, message.c_str(), strlen(message.c_str())) >= 0)
+                                {
+                                        waitforanswer = 1;
+                                        arduino_queue.pop();
+                                }
+                                else 
+                                {
+                                        printf (" Writing to arduino failed, device %s closed...\n", devicename);
+                                        close (fd);
+                                        fd = -1;
+                                }
                         }
                     }
 
